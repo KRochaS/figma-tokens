316950b52ddc4e0e570a158872992de548e0da0d
KRochaS <karinerocha.s@hotmail.com>
2023-05-01 16:50:06 -0300

    figma-tokens

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 01 May 2023 19:46:53 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#27272a",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8b5cf6"
+};
